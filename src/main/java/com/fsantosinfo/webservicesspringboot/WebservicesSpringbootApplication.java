ea0531fe9f7323e34e4f3206a76e82dcddf0ec43
Fabio Alves <fsantosinfo@gmail.com>
2020-10-03 15:33:44 -0300

    Project created

---
+++ src/main/java/com/fsantosinfo/webservicesspringboot/WebservicesSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.fsantosinfo.webservicesspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebservicesSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebservicesSpringbootApplication.class, args);
+	}
+
+}
